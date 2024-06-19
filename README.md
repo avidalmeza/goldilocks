a332abd78cc4d6816e96b34c8657b3be7fd36984
avidalmeza <vidalmeza.alessandra0@gmail.com>
2024-06-18 22:02:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Instructions
+Open the command prompt or terminal, navigate to the directory containing this script, and use the command  `python goldilocks.py` to execute the script.
---
+++ goldilocks.py
@@ -0,0 +1,261 @@
+# goldilocks.py
+
+import os
+import re
+import numpy as np
+import CifFile
+import mantid as mtd
+from mantid.simpleapi import CreateSampleWorkspace, SetSample
+from mantid.kernel import Material
+
+# Define read_cif function
+def read_cif(filepath):
+    # Read Crystallographic Information File
+    cif = CifFile.ReadCif(filepath)
+
+    # Define keys of interest
+    keys = ['_chemical_formula_sum', '_cell_length_a', '_cell_length_b', '_cell_length_c', '_cell_angle_alpha', '_cell_angle_beta', '_cell_angle_gamma', '_cell_volume', '_cell_formula_units_Z']
+
+    # Initialize empty dictionary
+    cif_dict = {}
+
+    # Iterate over each block in CIF
+    for database_code_PCD, block in cif.items():
+        # Print Pearson's Crystal Data browser code
+        print(f'Block: {database_code_PCD}')
+    
+        # Initialize empty dictionary
+        block_data = {}
+
+        # Iterate through keys of interest
+        for i in keys:
+            # Check key exists in block
+            if i in block:
+                # Store key in dictionary
+                block_data[i] = block[i]
+            # If key does not exist in block, print message
+            else:
+                # Store `None` in dictionary
+                block_data[i] = None
+                print(f'{i}: Not found in {database_code_PCD}.')
+
+        # Add the block_data dictionary to the information dictionary
+        cif_dict[database_code_PCD] = block_data
+
+    # Check for index troubleshooting
+    # for database_code_PCD, block_data in cif_dict.items():
+        # Print Pearson's Crystal Data browser code
+        # print(f'Block: {database_code_PCD}')
+
+    # Print value for each key of interest
+    for key, value in block_data.items():
+        print(f'{key}: {value}')
+        print()
+
+    # Define regular expression to replace text within parentheses with empty string
+    def remove_parentheses(i):
+        # \( matches an opening parenthesis
+        # \) matches a closing parenthesis
+        # [^)]* matches any character except a closing parenthesis
+        return re.sub(r'\([^)]*\)', '', i) if i else None
+
+    # Extract variables of interest
+    a = remove_parentheses(cif_dict[database_code_PCD]['_cell_length_a'])
+    b = remove_parentheses(cif_dict[database_code_PCD]['_cell_length_b'])
+    c = remove_parentheses(cif_dict[database_code_PCD]['_cell_length_c'])
+    alpha = cif_dict[database_code_PCD]['_cell_angle_alpha']
+    beta = cif_dict[database_code_PCD]['_cell_angle_beta']
+    gamma = cif_dict[database_code_PCD]['_cell_angle_gamma']
+    Z_param = cif_dict[database_code_PCD]['_cell_formula_units_Z']
+    formula = cif_dict[database_code_PCD]['_chemical_formula_sum']
+    cell_volume = cif_dict[database_code_PCD]['_cell_volume']
+
+    # Find sample number density
+    # Number density of sample in number of atoms or formula units per cubic Angstrom
+    sample_n_density = eval(Z_param)/eval(cell_volume)
+
+    # Split string by spaces
+    formula_split = formula.split() if formula else []
+
+    # Initialize empty dictionary
+    elements = {}
+
+    # Define regular expression pattern for dictionary
+    # Group 1: [A-Za-z] matches any uppercase (A-Z) or lowercase (a-z) letter
+    # Group 2: (\d+), where \d matches any digit (0-9) 
+    pattern = re.compile(r'([A-Za-z]+)(\d+)')
+
+    # Iterate through each element; return both index (i) and string for each element in chemical formula
+    for i, string in enumerate(formula_split):
+        # Match string against regular expression pattern
+        match = pattern.match(string)
+    
+        # Check object is not `None`
+        if match:
+            # Extract element symbol/Group 1 match
+            element_symbol = match.group(1)
+
+            # Extract subscript/Group 2 match, if `None` (no subscript) then `1`
+            # `None` is unlikely when retrieving from '_chemical_formula_sum' in CIF
+            subscript = match.group(2) if match.group(2) else '1'
+
+            # Find how many numbers per formula units
+            n_fu = eval(Z_param)*eval(subscript)
+
+            # Create new entry in dictionary for each element
+            elements[f'element_{i+1}'] = {'symbol': element_symbol, 'subscript': subscript, 'n_fu': n_fu}
+
+    # Print dictionary for troubleshooting
+    # for key, value in elements.items():
+        # print(f'{key}: {value}')
+
+    # Initialize empty array
+    array = []
+
+    # Iterate over each item in dictionary
+    for key, value in elements.items():
+        # Retrieve element symbol and numbers per formula units 
+        symbol = value['symbol']
+        n_fu = value['n_fu']
+
+        # Concatenate and append to array
+        concatenate = f'{symbol}{n_fu}'
+        array.append(concatenate)
+
+    # Print array for troubleshooting
+    # print(array)
+
+    # Concatenate array to string
+    mantid_formula = '-'.join([str(i) for i in array])
+
+    # Print string for troubleshooting
+    # print(mantid_formula)
+
+    # Initialize variable
+    total_n = 0
+
+    # Iterate over dictionary items
+    for key, value in elements.items():
+        # Extract numbers per formula units value and add to total sum
+        n_fu = value['n_fu']
+        total_n += n_fu
+
+    # Print total n
+    # print('Total n:', total_n)
+    
+    return mantid_formula, sample_n_density, total_n, a, b, c, alpha, beta, gamma
+
+# Define cross-section calculator
+def xs_calculator(arg1, neutron_energy, pack_fraction, can = ['flat', 'cyl', 'annular'], can_material = 'aluminum', a = None, b = None, c = None, alpha = None, beta = None, gamma = None, Z_param = None):
+    # Define pattern for Mantid-approved formula
+    mantid_format = r'^(\([A-Za-z][a-z]*\d*\)\d*|[A-Za-z][a-z]*\d*)(-[A-Za-z][a-z]*\d*|\([A-Za-z][a-z]*\d*\)\d*)*$'
+
+    # Define valid file extension
+    file_extension = '.cif'
+    
+    # Define validate string
+    def validate_formula(format, formula):
+        if not re.fullmatch(format, formula):
+            # Flags an error if formula (string) does not match format (pattern)
+            raise ValueError(f'The string is not in the accepted format.')
+        # Return 'True' if formula (string) matches format (pattern), False otherwise
+        return True
+
+    # Check if arg1 is a file path for a CIF
+    try:
+        if arg1.endswith(file_extension) and os.path.isfile(arg1):
+            read_cif(arg1)
+    # If error, check if arg1 is string and validate
+    except ValueError:
+        if validate_formula(mantid_format, arg1):
+            mantid_formula = arg1
+        else:
+            print(f'{arg1} is neither a valid Mantid formula nor a CIF.')
+
+    # Check version for troubleshooting
+    # mantid_version = mtd.__version__
+    # print(mantid_version)
+
+    # Create empty data container/workspace
+    ws = CreateSampleWorkspace()
+
+    # Check data container/workspace for troubleshooting
+    # if ws.id() == 'Workspace2D':
+    #    print(ws.name() + ' is an ' + ws.id())
+
+    # Print formula for troubleshooting
+    # print(mantid_formula)
+
+    # Define sample
+    # Add material to data container/workspace
+    SetSample(ws, Material = {'ChemicalFormula': mantid_formula,
+                              'SampleNumberDensity': sample_n_density})
+
+    # Check for troubleshooting
+    sample = ws.sample()
+
+    # Retrieve absorption cross-section
+    absorb_xs = sample.getMaterial().absorbXSection()*total_n
+    # print(f'Absorption cross-section: {absorb_xs}')
+
+    # Retrieve total scattering cross-section
+    # Define scattering cross section per formula unit in bn/fu
+    scatter_xs = sample.getMaterial().totalScatterXSection()*total_n
+    # print(f'Total scattering cross-section: {scatter_xs}')
+
+    # Retrieve coherent scattering length
+    scatter_length = sample.getMaterial().cohScatterLength()
+    # print(f'Coherent scattering length: {scatter_length}')
+
+    # Retrieve relative molecular mass
+    # Define molecular mass in g/mol/fu
+    molecular_mass = sample.getMaterial().relativeMolecularMass()
+    # print(f'Relative molecular mass: {molecular_mass}')
+
+    # Convert string values to floats
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    alpha = float(alpha)
+    beta = float(beta)
+    gamma = float(gamma)
+    
+    # Find unit cell volume in A^3
+    unit_cell_volume = a * b * c * np.sqrt(1-np.cos(alpha*np.pi/180)**2 - np.cos(beta*np.pi/180)**2 - np.cos(gamma*np.pi/180)**2 + 2*np.cos(alpha*np.pi/180) * np.cos(beta*np.pi/180) * np.cos(gamma*np.pi/180))
+    
+    # Find theoretical density in g/cc
+    theory_density = molecular_mass/unit_cell_volume/0.6022
+    
+    # Find absorption cross section per formula unit at given neutron energy in bn/fu
+    sqrt_neutron_energy = np.sqrt(25/neutron_energy)
+    absorb_xs_sqrt = absorb_xs*sqrt_neutron_energy
+    
+    # Find sample weight
+    sample_mass = can_volume*pack_fraction
+    
+    # Find sample volume in cc
+    sample_volume = sample_mass/theory_density/pack_fraction
+    
+    # Find number of moles of formula unit in sample
+    sample_moles = sample_mass/molecular_mass
+    
+    # Find penetration depth due to scattering in cm
+    scatter_depth = unit_cell_volume/(scatter_xs*pack_fraction)
+    
+    # Find penetration depth due to absorption in cm
+    absorb_depth = unit_cell_volume/(absorb_xs_sqrt*pack_fraction)
+    
+    # Find total penetration depth due to scattering and absorption in cm
+    total_depth = unit_cell_volume/((scatter_xs + absorb_xs_sqrt)*pack_fraction)
+    
+    # Find thickness of sample spread homogenously over sample can in cm
+    sample_thick = sample_mass/(pack_fraction*theory_density*can_volume)
+    
+    # Find percent of incident beam that is scattered (assume no absorption)
+    percent_scatter = 100 * (1-(np.exp(-(sample_thick/scatter_depth))))
+    
+    # Find percent of incident beam that is absorbed (assume no scattering)
+    percent_absorb =  100 * (1-(np.exp(-(sample_thick/absorb_depth))))
+    
+    # Find thickness of a 10 percent scatterer in cm
+    scatter_thick = np.log(0.9)*scatter_depth
